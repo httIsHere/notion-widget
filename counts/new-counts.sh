a1286c16b7dd1768c59cc5ef7e7b6655141d9965
TinaHuang0183 <Tina.Huang@splashtop.com>
2024-11-07 09:11:43 +0800

    feat: gtd counter;

--- counts/new-counts.sh
+++ counts/new-counts.sh
@@ -2,9 +2,11 @@
  # @Author: Tina Huang
  # @Date: 2024-08-01 14:35:36
  # @LastEditors: Tina Huang
- # @LastEditTime: 2024-08-01 14:57:22
+ # @LastEditTime: 2024-11-07 09:10:12
  # @Description: 
 ### 
+
+# 请依次输入文件名，模版名
 
 echo "目标的文件名：$1，目标模版名为：$2";
 echo "创建新的模版统计组件....";
